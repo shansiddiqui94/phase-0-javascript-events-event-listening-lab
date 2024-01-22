e8442e6a1cf8e5b4c6fd7b975dc53d8e87bb73bb
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-01-21 23:37:20 -0500

    eventlistener lab complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+const input = document.getElementById('button'); // I am grabbing the element called Button
+
+function clickAlert() { //I create a function called CLickAlert that returns a message stating "I been clicked" 
+  alert('I was clicked!'); // This same function can be anonymous as well. 
 }
+
+input.addEventListener('click', clickAlert); //here I add my event listener to my input and pass my two arguments
+                                            //1. listen for the click. 2. once button is clicked pass that call back function clickAlert
+}
+
+addingEventListener()
+//Call the function 
